1fa4cf44d9b3237ecd139a55df71d428cc4d5c1f
文强 柯 <709575581@qq.com>
2026-01-05 10:47:03 +0800

    feat(算法核心服务): 添加预测和规划模块到算法实现
    将prediction和programme模块添加到算法核心服务的实现中，并更新__all__列表以包含新模块

--- src/algo_core_service/algorithms/__init__.py
+++ src/algo_core_service/algorithms/__init__.py
@@ -1,5 +1,5 @@
 """Algorithm implementations for the core service."""
 
-from . import prepare
+from . import prediction, prepare, programme
 
-__all__ = ["prepare"]
+__all__ = ["prepare", "prediction", "programme"]
